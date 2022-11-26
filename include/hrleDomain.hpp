17593566da0663f87095671087c724208677d89a
Julius Piso <julius@piso.at>
2022-11-26 15:54:55 +0100

    Bug fixes for Windows

--- include/hrleDomain.hpp
+++ include/hrleDomain.hpp
@@ -826,7 +826,7 @@ public:
               unsigned long long relativeId = 0;
               stream.read(reinterpret_cast<char *>(&relativeId), bytesPerIndex);
               definedId += relativeId;
-              runTypes[counter] = static_convert<hrleSizeType>(definedId);
+              runTypes[counter] = static_cast<hrleSizeType>(definedId);
             }
             ++counter;
           }
